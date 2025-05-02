3d2833273b96f9075ef831e39d8b3586549bf91c
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-02 16:16:25 +0300

    add

---
+++ 001.FirstStepsInCodingLab/004.InchesToCentimeters/InchesToCentimeters.cs
@@ -0,0 +1,8 @@
+﻿//First steps in coding - lab, Task 004
+//02.05.2025, 15:57
+
+double inches = double.Parse(Console.ReadLine());
+
+double centimeters = inches * 2.54;
+
+Console.WriteLine(centimeters);
