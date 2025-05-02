038ec12bc273327dedbb23a9f7c6ea1af6c3253c
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-03 00:51:35 +0300

    add

---
+++ 002.FirstStepsInCodingExercise/007.FoodDelivery/FoodDelivery.cs
@@ -0,0 +1,12 @@
+﻿//First steps in coding - exercise, Task 007
+//03.05.2025, 00:21
+
+int chikenCnt = int.Parse(Console.ReadLine());
+int fishCnt = int.Parse(Console.ReadLine());
+int vegetableCnt = int.Parse(Console.ReadLine());
+
+double price = chikenCnt * 10.35 + fishCnt * 12.40 + vegetableCnt * 8.15;
+double desertPrice = price * 0.20;
+double finalPrice = price + desertPrice + 2.50;
+
+Console.WriteLine(finalPrice);
