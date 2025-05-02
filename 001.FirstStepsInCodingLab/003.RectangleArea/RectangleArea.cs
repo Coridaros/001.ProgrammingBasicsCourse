66e48988575c90aa61a0cc3b014914061d9db195
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-02 16:16:08 +0300

    add

---
+++ 001.FirstStepsInCodingLab/003.RectangleArea/RectangleArea.cs
@@ -0,0 +1,9 @@
+﻿//First steps in coding - lab, Task 003
+//02.05.2025, 15:54
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int area = a * b;
+
+Console.WriteLine(area);
