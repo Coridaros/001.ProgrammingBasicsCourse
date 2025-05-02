a2286919a0b0f614e5a3148023f37e457461cb62
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-03 00:50:56 +0300

    add

---
+++ 002.FirstStepsInCodingExercise/005.SuppliesForSchool/SuppliesForSchool.cs
@@ -0,0 +1,13 @@
+﻿//First steps in coding - exercise, Task 005
+//02.05.2025, 20:49
+
+int penCnt = int.Parse(Console.ReadLine());
+int markeCnt = int.Parse(Console.ReadLine());
+int cleanerLitters = int.Parse(Console.ReadLine());
+int discountPercent = int.Parse(Console.ReadLine());
+
+double nMoney = penCnt * 5.80 + markeCnt * 7.2 + cleanerLitters * 1.20;
+double discount = nMoney * (discountPercent / 100.0);
+nMoney -= discount;
+
+Console.WriteLine(nMoney);
