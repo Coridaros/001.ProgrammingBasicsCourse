f19f07aad3e427d9d466a7259c1b276de11b2c7e
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-03 00:50:16 +0300

    add

---
+++ 002.FirstStepsInCodingExercise/003.DepositCalculator/DepositCalculator.cs
@@ -0,0 +1,12 @@
+﻿//First steps in coding - exercise, Task 003
+//02.05.2025, 18:19
+
+double deposit = double.Parse(Console.ReadLine());
+int period = int.Parse(Console.ReadLine());
+double vat = double.Parse(Console.ReadLine());
+
+double addedVat = deposit * (vat / 100.0);
+double monthVat = addedVat / 12;
+double total = deposit + period * monthVat; ;
+
+Console.WriteLine(total);
