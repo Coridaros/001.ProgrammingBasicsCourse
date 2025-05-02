03136f529a92413c9e72825ca2a07040c4342161
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-03 00:49:54 +0300

    add

---
+++ 002.FirstStepsInCodingExercise/002.RadiansToDegrees/RadiansToDegrees.cs
@@ -0,0 +1,8 @@
+﻿//First steps in coding - exercise, Task 002
+//02.05.2025, 16:36
+
+double radians = double.Parse(Console.ReadLine());
+
+double deg = radians * 180 / Math.PI;
+
+Console.WriteLine(deg);
