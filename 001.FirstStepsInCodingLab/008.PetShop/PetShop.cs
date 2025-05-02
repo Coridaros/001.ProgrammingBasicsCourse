5e86e0b5aa62370d87be7d2e2dfb3ea54dac4a1c
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-02 16:17:39 +0300

    add

---
+++ 001.FirstStepsInCodingLab/008.PetShop/PetShop.cs
@@ -0,0 +1,9 @@
+﻿//First steps in coding - lab, Task 008
+//02.05.2025, 16:08
+
+int dogFoodCnt = int.Parse(Console.ReadLine());
+int catFoodCnt = int.Parse(Console.ReadLine());
+
+double price = dogFoodCnt * 2.50 + catFoodCnt * 4;
+
+Console.WriteLine($"{price} lv.");
