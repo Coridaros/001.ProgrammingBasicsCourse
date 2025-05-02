c50b37273292314a6ee35af1c52cde8b4d395748
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-03 00:52:12 +0300

    add

---
+++ 002.FirstStepsInCodingExercise/009.FishTank/FishTank.cs
@@ -0,0 +1,12 @@
+﻿//First steps in coding - exercise, Task 009
+//03.05.2025, 00:43
+
+int len = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double percent = double.Parse(Console.ReadLine());
+
+double litters = (len * width * height) * 0.001;
+double nLitters = litters * (1 - (percent / 100.0));
+
+Console.WriteLine(nLitters);
