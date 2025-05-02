23bc1e7be54a7268e9c6d5fc043a4ead2c7909a5
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-03 00:51:52 +0300

    add

---
+++ 002.FirstStepsInCodingExercise/008.BasketballEquipment/BasketballEquipment.cs
@@ -0,0 +1,13 @@
+﻿//First steps in coding - exercise, Task 008
+//03.05.2025, 00:34
+
+int tax = int.Parse(Console.ReadLine());
+
+double shoesPrice = tax - (tax * 0.40);
+double equipPrice = shoesPrice - (shoesPrice * 0.20);
+double ballPrice = equipPrice / 4;
+double accPrice = ballPrice / 5;
+
+double finalPrice = shoesPrice + equipPrice + ballPrice + accPrice + tax;
+
+Console.WriteLine(finalPrice);
