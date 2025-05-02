561e7601cbe1706478328a473b5dbc4d305e2f26
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-02 16:18:00 +0300

    add

---
+++ 001.FirstStepsInCodingLab/009.YardGreening/YardGreening.cs
@@ -0,0 +1,11 @@
+﻿//First steps in coding - lab, Task 009
+//02.05.2025, 16:12
+
+double area = double.Parse(Console.ReadLine());
+
+double price = area * 7.61;
+double discount = price * 0.18;
+price -= discount;
+
+Console.WriteLine($"The final price is: {price} lv.");
+Console.WriteLine($"The discount is: {discount} lv.");
