04eec8956c367051074b36a8bb4bca4e6d0c42de
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-10 01:27:56 +0300

    add

---
+++ 004.ConditionalStatementsExercise/001.SumSeconds/SumSeconds.cs
@@ -0,0 +1,20 @@
+﻿//Conditional statements - exercise, Task 001
+//09.05.2025, 22:59
+
+int firstCompetitor = int.Parse(Console.ReadLine());
+int secondCompetitor = int.Parse(Console.ReadLine());
+int thirdCompetitor = int.Parse(Console.ReadLine());
+
+int totalSeconds = firstCompetitor + secondCompetitor + thirdCompetitor;
+
+int minutes = totalSeconds / 60;
+int seconds = totalSeconds % 60;
+
+if(seconds < 10)
+{
+    Console.WriteLine($"{minutes}:0{seconds}");
+}
+else
+{
+    Console.WriteLine($"{minutes}:{seconds}");
+}
