f67dc83f38d3ae31aaeb6bb304637b09f1877cab
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-09 22:55:04 +0300

    add

---
+++ 003.ConditionalStatementsLab/003.EvenOrOdd/EvenOrOdd.cs
@@ -0,0 +1,13 @@
+﻿//Conditional statements - lab, Task 003
+//03.05.2025, 01:05
+
+int number = int.Parse(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("even");
+}
+else
+{
+    Console.WriteLine("odd");
+}
