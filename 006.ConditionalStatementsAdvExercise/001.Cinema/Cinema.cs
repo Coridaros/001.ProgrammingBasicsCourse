d95b7ff5fe33d0802d33beef1a33d1bd33d95fcd
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-12 00:27:18 +0300

    add

---
+++ 006.ConditionalStatementsAdvExercise/001.Cinema/Cinema.cs
@@ -0,0 +1,19 @@
+﻿//Conditional statements advanced - exercise, Task 001
+//10.05.2025, 14:03
+
+string type = Console.ReadLine().ToLower();
+int r = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (type == "premiere")
+{
+    Console.WriteLine($"{(r * c * 12.0):F2} leva");
+}
+else if (type == "normal")
+{
+    Console.WriteLine($"{(r * c * 7.50):F2} leva");
+}
+else if (type == "discount")
+{
+    Console.WriteLine($"{(r * c * 5.0):F2} leva");
+}
