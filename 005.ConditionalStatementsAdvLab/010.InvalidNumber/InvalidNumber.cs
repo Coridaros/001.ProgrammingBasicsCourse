377eff53e2b195d91eda27f79a2783ac3d76f5e4
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-11 15:23:54 +0300

    add

---
+++ 005.ConditionalStatementsAdvLab/010.InvalidNumber/InvalidNumber.cs
@@ -0,0 +1,11 @@
+﻿//Conditional statements advanced - lab, Task 010
+//10.05.2025, 13:10
+
+int number = int.Parse(Console.ReadLine());
+
+bool isValid = (number >= 100 && number <= 200) || number == 0;
+
+if(!isValid)
+{
+    Console.WriteLine("invalid");
+}
