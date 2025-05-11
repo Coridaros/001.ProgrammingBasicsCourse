7f3702723ee16722d9451ca694d5d55956ef5b57
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-11 15:22:56 +0300

    add

---
+++ 005.ConditionalStatementsAdvLab/007.WorkingHours/WorkingHours.cs
@@ -0,0 +1,25 @@
+﻿//Conditional statements advanced - lab, Task 007
+//10.05.2025, 12:52
+
+int hour = int.Parse(Console.ReadLine());
+string day = Console.ReadLine().ToLower();
+
+string result = string.Empty;
+
+if(hour >= 10 && hour <= 18)
+{
+    if(day != "sunday")
+    {
+        result = "open";
+    }
+    else
+    {
+        result = "closed";
+    }
+}
+else
+{
+    result = "closed";
+}
+
+Console.WriteLine(result);
