b52269f0234060edcf923f098e4dbb9b0f6480a9
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-11 15:23:16 +0300

    add

---
+++ 005.ConditionalStatementsAdvLab/008.CinemaTicket/CinemaTicket.cs
@@ -0,0 +1,32 @@
+﻿//Conditional statements advanced - lab, Task 008
+//10.05.2025, 12:59
+
+string day = Console.ReadLine();
+
+switch(day)
+{
+    case "Monday":
+        Console.WriteLine(12);
+        break;
+    case "Tuesday":
+        Console.WriteLine(12);
+        break;
+    case "Wednesday":
+        Console.WriteLine(14);
+        break;
+    case "Thursday":
+        Console.WriteLine(14);
+        break;
+    case "Friday":
+        Console.WriteLine(12);
+        break;
+    case "Saturday":
+        Console.WriteLine(16);
+        break;
+    case "Sunday":
+        Console.WriteLine(16);
+        break;
+    default:
+        break;
+
+}
