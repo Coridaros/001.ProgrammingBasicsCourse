847f6bb6e2cbe9a052046195f198e30ca911142f
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-11 15:23:36 +0300

    add

---
+++ 005.ConditionalStatementsAdvLab/009.FruitOrVegetable/FruitOrVegetable.cs
@@ -0,0 +1,25 @@
+﻿//Conditional statements advanced - lab, Task 009
+//10.05.2025, 13:05
+
+string product = Console.ReadLine();
+
+switch(product)
+{
+    case "banana":
+    case "apple":
+    case "kiwi":
+    case "cherry":
+    case "lemon":
+    case "grapes":
+        Console.WriteLine("fruit");
+        break;
+    case "tomato":
+    case "cucumber":
+    case "pepper":
+    case "carrot":
+        Console.WriteLine("vegetable");
+        break;
+    default:
+        Console.WriteLine("unknown");
+        break;
+}
