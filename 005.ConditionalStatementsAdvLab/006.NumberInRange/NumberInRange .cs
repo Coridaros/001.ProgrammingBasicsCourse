8576c20748287d188f244de35d1d49a86f69dec5
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-11 15:22:34 +0300

    add

---
+++ 005.ConditionalStatementsAdvLab/006.NumberInRange/NumberInRange .cs
@@ -0,0 +1,15 @@
+﻿//Conditional statements advanced - lab, Task 006
+//10.05.2025, 12:49
+
+int number = int.Parse(Console.ReadLine());
+
+bool inRange = (number >= -100 && number <= 100) && number != 0;
+
+if(inRange)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
