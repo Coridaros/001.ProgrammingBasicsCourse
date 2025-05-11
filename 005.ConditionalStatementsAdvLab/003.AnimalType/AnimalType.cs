4789e107e2fb3c5df84b06ec507c43b87bf71551
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-11 15:18:42 +0300

    add

---
+++ 005.ConditionalStatementsAdvLab/003.AnimalType/AnimalType.cs
@@ -0,0 +1,19 @@
+﻿//Conditional statements advanced - lab, Task 003
+//10.05.2025, 02:02
+
+string animal = Console.ReadLine();
+
+switch(animal)
+{
+    case "dog":
+        Console.WriteLine("mammal");
+        break;
+    case "crocodile":
+    case "tortoise":
+    case "snake":
+        Console.WriteLine("reptile");
+        break;
+    default:
+        Console.WriteLine("unknown");
+        break;
+}
