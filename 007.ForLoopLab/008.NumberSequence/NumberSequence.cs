b6cb4c9e30996d656bd32a363a374c742353b04b
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-13 21:49:57 +0300

    add

---
+++ 007.ForLoopLab/008.NumberSequence/NumberSequence.cs
@@ -0,0 +1,25 @@
+﻿//For loop - lab, Task 008
+//11.05.2025, 22:44
+
+int n = int.Parse(Console.ReadLine());
+
+int max = int.MinValue;
+int min = int.MaxValue;
+
+for(int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (number > max)
+    {
+        max = number;
+    }
+
+    if (number < min)
+    {
+        min = number;
+    }
+}
+
+Console.WriteLine($"Max number: {max}");
+Console.WriteLine($"Min number: {min}");
