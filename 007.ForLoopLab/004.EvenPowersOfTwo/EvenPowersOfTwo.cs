558a943f09924816735b773b36926da0abb5e912
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-13 21:48:29 +0300

    add

---
+++ 007.ForLoopLab/004.EvenPowersOfTwo/EvenPowersOfTwo.cs
@@ -0,0 +1,13 @@
+﻿//For loop - lab, Task 004
+//11.05.2025, 22:24
+
+int n = int.Parse(Console.ReadLine());
+
+int number = 1;
+
+for(int i = 0; i < n; i++)
+{
+    Console.WriteLine(number);
+
+    number = number * 2 * 2;
+}
