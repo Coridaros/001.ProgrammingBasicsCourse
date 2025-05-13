0531debd1623abf07c78aa11647d2b2505a56526
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-13 21:49:13 +0300

    add

---
+++ 007.ForLoopLab/006.VowelsSum/VowelsSum.cs
@@ -0,0 +1,34 @@
+﻿//For loop - lab, Task 006
+//11.05.2025, 22:37
+
+string text = Console.ReadLine();
+
+int sum = 0;
+
+for(int i = 0; i < text.Length; i++)
+{
+    char letter = text[i];
+
+    if (letter == 'a')
+    {
+        sum += 1;
+    }
+    else if(letter == 'e')
+    {
+        sum += 2;
+    }
+    else if(letter == 'i')
+    {
+        sum += 3;
+    }
+    else if(letter == 'o')
+    {
+        sum += 4;
+    }
+    else if(letter == 'u')
+    {
+        sum += 5;
+    }
+}
+
+Console.WriteLine(sum);
