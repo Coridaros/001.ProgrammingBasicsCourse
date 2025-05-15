24f86bd957ecb7cde9e83c243380f0d993b60688
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-15 17:55:53 +0300

    add

---
+++ 008.ForLoopExercise/002.HalfSumElement/HalfSumElement.cs
@@ -0,0 +1,33 @@
+﻿//For loop - exercise, Task 002
+//12.05.2025, 22:46
+
+int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int max = int.MinValue;
+
+for(int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    sum += number;
+
+    if(number > max)
+    {
+        max = number;
+    }
+}
+
+int sumWithoutMaxNumber = sum - max;
+
+if(max == sumWithoutMaxNumber)
+{
+    Console.WriteLine("Yes");
+    Console.WriteLine("Sum = " + max);
+}
+else
+{
+    int diff = Math.Abs(max - sumWithoutMaxNumber);
+    Console.WriteLine("No");
+    Console.WriteLine($"Diff = " + diff);
+}
